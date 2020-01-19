eee927ebd38f11f71cf9bc79f6790ae75c29165f
murilo_r <murilo_r@dell.com>
2020-01-19 15:28:41 -0300

    creating the first api

---
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data // getters and setters
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // builder pattern
+public class BeerDto {
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
