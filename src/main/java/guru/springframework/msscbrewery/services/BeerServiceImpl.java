d9a7cfeae73b273ff97c1a949d62163a2b5411f6
murilobauerc <panchobauer@gmail.com>
2020-01-19 19:13:26 -0300

    creating the customer api

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -9,7 +9,8 @@ import java.util.UUID;
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
-        return BeerDto.builder().id(UUID.randomUUID())
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
